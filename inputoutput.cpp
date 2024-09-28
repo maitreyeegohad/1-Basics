c780042e144b8a812a5f89bf7f11df527ab478a6
Maitreyee Gohad <maitreyee.gohad@gmail.com>
2024-09-28 12:05:02 +0530

    Create inputoutput.cpp

---
+++ inputoutput.cpp
@@ -0,0 +1,38 @@
+//Maitreyee Gohad
+//24070123509
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	char b;
+	float c;
+	
+	cout<<"Enter an integer:"<<endl;
+	cin>>a;
+	cout<<"Enter a letter:"<<endl;
+	cin>>b;
+	cout<<"Enter a fractional number:"<<endl;
+	cin>>c;
+	
+	cout<<"\n\nPrinting the vaules: \n\n"<<endl;
+	cout<<a<<endl<<b<<endl<<c<<endl;
+	return 0;
+}
+
+/*OUTPUT:
+Enter an integer:
+9
+Enter a letter:
+k
+Enter a fractional number:
+7.885
+
+
+Printing the vaules: 
+
+
+9
+k
+7.885*/
